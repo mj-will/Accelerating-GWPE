f05ffc8899df5766fc3bffbdc7c919c2f0995a18
mj-will <michael.williams@zxilix.com>
2019-01-22 15:37:44 +0000

    Add script for plotting hdf results files

---
+++ nn/plot_results.py
@@ -0,0 +1,9 @@
+
+from utils import make_plots_multiple
+
+def main():
+    make_plots_multiple('./outdir/iota_psi_dist_ra_dec_marg_phase/run2/', './misc_plots/', blocks = "all")
+
+if __name__ == '__main__':
+    main()
+
