f13c099575ca74caf1b94370af7138b5d006444b
mj-will <michael.williams@zxilix.com>
2019-01-29 16:14:22 +0000

    Add command line options

--- nn/plot_results.py
+++ nn/plot_results.py
@@ -3,7 +3,6 @@ import sys
 from utils import make_plots_multiple
 
 def main():
-    print(len(sys.argv))
     inputdir = sys.argv[1]
     outputdir = sys.argv[2]
     make_plots_multiple(inputdir, outputdir, blocks = "all")
