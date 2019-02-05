5ecf5b056a5f7efa10bec8493ee0a27b7a0f17be
mj-will <michael.williams@zxilix.com>
2019-02-05 14:55:30 +0000

    Add search over two parameters

--- nn/test_models.py
+++ nn/test_models.py
@@ -15,7 +15,6 @@ def update_model(params, model_path='auto_model.json'):
 
     with open(model_path, 'r') as model_file:
         model_params = json.load(model_file)
-        print(model_params)
     # check specified parameters are model parameters
     for key in params.keys():
         if key in model_params.keys():
@@ -28,17 +27,16 @@ def update_model(params, model_path='auto_model.json'):
 
 def main():
     # parameters to test
-    parameters = {'neurons': [10, 20, 50, 100, 250, 500]}
+    parameters = {'neurons': [600, 750, 900, 1050, 1200], 'layers': [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 18, 21, 24, 27, 30]}
     model_path = 'auto_model.json'
-
-    for key, value in parameters.iteritems():
-        for v in value:
+    for n in parameters['neurons']:
+        for l in parameters['layers']:
             # update model
-            d = {key: v}
+            d = {'neurons': n, 'layers': l}
             update_model(d, model_path)
-            print('Running nn for {}: {}'.format(key, v))
+            print('Running nn for {} neurons - {} layers'.format(n, l))
             # run model
-            #os.system('python nn.py ' +  model_path)
+            os.system('python nn.py ' +  model_path)
 
 if __name__ == '__main__':
     main()
