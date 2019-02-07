c5fc9c1b19fcae6f5d92aa2878786d93f2ab813e
mj-will <michael.williams@zxilix.com>
2019-02-07 11:30:05 +0000

    Set tensorflow to only used memory as needed

--- nn/function_approximator.py
+++ nn/function_approximator.py
@@ -7,8 +7,19 @@ import deepdish
 
 from keras.optimizers import SGD, RMSprop, Adam
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint, EarlyStopping
+import tensorflow as tf
+import keras.backend as K
 
 import utils
+
+# TensorFlow wizardry
+config = tf.ConfigProto()
+# Don't pre-allocate memory; allocate as-needed
+config.gpu_options.allow_growth = True
+# Only allow a total of half the GPU memory to be allocated
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+# Create a session with the above options specified.
+K.tensorflow_backend.set_session(tf.Session(config=config))
 
 class FunctionApproximator(object):
 
