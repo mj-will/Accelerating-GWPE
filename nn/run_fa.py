c780580a4b70f43db718e15b55970532e047b5ac
mj-will <michael.williams@zxilix.com>
2019-02-18 18:13:47 +0000

    Add split option to config

--- nn/run_fa.py
+++ nn/run_fa.py
@@ -25,23 +25,33 @@ def main():
     n_extrinsic = np.shape(priors_ex)[-1]
     n_intrinsic = np.shape(priors_in)[-1]
     if n_intrinsic == 0:
-        n_inputs = [n_extrinsic]
+        input_shape = [n_extrinsic]
     elif n_extrinsic == 0:
-        n_inputs = [n_intrinsic]
+        input_shape = [n_intrinsic]
     else:
-        n_inputs = sum([n_extrinsic, n_intrinsic])
-    FA = FunctionApproximator(n_inputs, json_file=model_path, parameter_names=data.parameters)
+        if params["split"]:
+            # ordering is important if using split inputs
+            print("\nTraining function approximator with split inputs\n")
+            input_shape = [n_extrinsic, n_intrinsic]
+        else:
+            input_shape = sum([n_extrinsic, n_intrinsic])
+    # the function approximator class should be able handle any of the previous input shapes
+    FA = FunctionApproximator(input_shape, json_file=model_path, parameter_names=data.parameters)
+    # FA will split inputs/priors automatically depending on how it was setup
     priors = np.concatenate([priors_ex, priors_in], axis=-1)
     FA.setup_normalisation(priors)
     data.prep_data_chain(block_size=params["block_size"], norm_logL=False, norm_intrinsic=False, norm_extrinsic=False)
     if params["blocks"] == "all":
+        # avoid the first block with very large negative values of logL
+        # -> start at 1
         blocks_2_train = range(1, data.N_blocks)
     else:
         blocks_2_train = params["blocks"]
     print("Training on blocks: ", blocks_2_train)
-
+    # input data need not be split but I wrote it like this originally don't see the need to change it
     for x_in, x_ex, y, i in zip(data.intrinsic_parameters, data.extrinsic_parameters, data.logL, range(data.N_blocks)):
         if i in blocks_2_train:
+            # make sure the input data isn't split
             if x_ex.any() and x_in.any():
                 x = np.concatenate([x_ex, x_in], axis=-1)
             elif x_ex.any():
